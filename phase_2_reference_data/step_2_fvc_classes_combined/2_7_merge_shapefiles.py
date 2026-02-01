cc1ed8b0247a86d282ec8ecb843fc44acc2533e9
lnsotom <lauransotomayor5@gmail.com>
2026-02-02 10:42:13 +1100

    npv and senescent

--- phase_2_reference_data/step_2_fvc_classes_combined/2_7_merge_shapefiles.py
+++ phase_2_reference_data/step_2_fvc_classes_combined/2_7_merge_shapefiles.py
@@ -21,18 +21,18 @@ def merge_shapefiles(folder_path):
 ## low
 # folder_path = '/media/laura/Extreme SSD/qgis/calperumResearch/site2_1_DD0011/inputs/masks/tiles_3072/raw/fvc_class/annotation_shp'
 ## medium
-# folder_path = '/media/laura/Extreme SSD/qgis/calperumResearch/site1_1_DD0001/inputs/masks/tiles_3072/raw/fvc_class/annotation_shp'  # Update this path to your folder
+folder_path = '/media/laura/Extreme SSD/qgis/calperumResearch/site1_1_DD0001/inputs/masks/tiles_3072/raw/fvc_class/annotation_shp'  # Update this path to your folder
 ## dense
-folder_path = '/media/laura/Extreme SSD/qgis/calperumResearch/site3_1_DD0012/inputs/masks/tiles_3072/raw/fvc_class/annotation_shp'
+# folder_path = '/media/laura/Extreme SSD/qgis/calperumResearch/site3_1_DD0012/inputs/masks/tiles_3072/raw/fvc_class/annotation_shp'
 merged_shapefiles = merge_shapefiles(folder_path)
 
 
 ## Save the merged GeoDataFrame to a new shapefile low
 # output_path = '/media/laura/Extreme SSD/qgis/calperumResearch/site2_1_DD0011/inputs/masks/tiles_3072/raw/fvc_class/annotation_shp/merged/fvc_class_SASMDD0011.shp'
 ## Save the merged GeoDataFrame to a new shapefile medium
-# output_path = '/media/laura/Extreme SSD/qgis/calperumResearch/site1_1_DD0001/inputs/masks/tiles_3072/raw/fvc_class/annotation_shp/merged/fvc_class_SASMDD0001.shp'
+output_path = '/media/laura/Extreme SSD/qgis/calperumResearch/site1_1_DD0001/inputs/masks/tiles_3072/raw/fvc_class/annotation_shp/merged/fvc_class_SASMDD0001.shp'
 ## Save the merged GeoDataFrame to a new shapefile dense
-output_path = '/media/laura/Extreme SSD/qgis/calperumResearch/site3_1_DD0012/inputs/masks/tiles_3072/raw/fvc_class/annotation_shp/merged/fvc_class_SASMDD0012.shp'
+# output_path = '/media/laura/Extreme SSD/qgis/calperumResearch/site3_1_DD0012/inputs/masks/tiles_3072/raw/fvc_class/annotation_shp/merged/fvc_class_SASMDD0012.shp'
 merged_shapefiles.to_file(output_path)
 
 print(f"Merged shapefiles saved to: {output_path}")
